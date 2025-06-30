9de9e75a9ea81c63a780cb7df1ea12b3c9ac4f69
minato0129 <gororyo0129@gmail.com>
2025-06-30 15:04:40 +0900

    userの名前検索の整形

--- controllers/users.go
+++ controllers/users.go
@@ -7,6 +7,7 @@ import (
 	"github.com/labstack/echo"
 )
 
+//名前を元にユーザーの情報を返す
 func GetUsersByName(ctx echo.Context) error {
 	name := ctx.Param("name")
 
@@ -18,7 +19,32 @@ func GetUsersByName(ctx echo.Context) error {
 	}
 
 	// サービスを呼び出す
-	result := services.GetUsers(name)
+	result := services.GetUsersByName(name)
+
+	// エラー処理
+	if result.Status != 200 {
+		return ctx.JSON(http.StatusBadRequest, echo.Map{
+			"error": result.Message,
+		})
+	}
+
+	return ctx.JSON(http.StatusOK, result.Data)
+}
+
+
+//idを元にユーザーの情報を返す
+func GetUsersById(ctx echo.Context) error {
+	id := ctx.Param("id")
+
+	// エラー処理
+	if id == "" {
+		return ctx.JSON(http.StatusBadRequest, echo.Map{
+			"error": services.UserNotFound,
+		})
+	}
+
+	// サービスを呼び出す
+	result := services.GetUsersById(id)
 
 	// エラー処理
 	if result.Status != 200 {
--- services/getuser.go
+++ services/getuser.go
@@ -5,8 +5,12 @@ import (
 	"net/http"
 )
 
+type returnUser struct {
+	Id    string      `json:"id"`
+	Name  string      `json:"name"`
+}
 // 名前を元にユーザー検索
-func GetUsers(name string) Result {
+func GetUsersByName(name string) Result {
 
 	results, err := models.GetUserByName(name)
 
@@ -21,6 +25,32 @@ func GetUsers(name string) Result {
 	return Result{
 		Message: "",
 		Status:  http.StatusOK,
-		Data:    results,
+		Data:    returnUser{
+			Id:   results.UserData.UserID,
+			Name: results.UserData.UserName,
+		},
+	}
+}
+
+// 名前を元にユーザー検索
+func GetUsersById(id string) Result {
+
+	results, err := models.GetUserByID(id)
+
+	if err != nil {
+		return Result{
+			Message: UserNotFound,
+			Status:  http.StatusNotFound,
+			Data:    nil,
+		}
+	}
+
+	return Result{
+		Message: "",
+		Status:  http.StatusOK,
+		Data:    returnUser{
+			Id:   results.UserData.UserID,
+			Name: results.UserData.UserName,
+		},
 	}
 }
