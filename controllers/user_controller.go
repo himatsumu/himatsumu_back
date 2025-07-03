839e2e7c372ea84d37948de9037e4795429bd40a
toma1128 <kirima--idea--@au.com>
2025-07-03 10:26:31 +0900

    [fix]レスポンスの返すときにmap[string][interface]を使用していたのをecho.Mapに変更

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -15,7 +15,7 @@ func CheckUser(ctx echo.Context) error {
 	uuid := ctx.Get("user_uuid").(string)
 	// UUIDが空の場合エラーを返す
 	if uuid == "" {
-		return ctx.JSON(http.StatusBadRequest, map[string]interface{}{
+		return ctx.JSON(http.StatusBadRequest, echo.Map{
 			"status":  http.StatusBadRequest,
 			"message": "UUIDが空です",
 		})
@@ -25,14 +25,14 @@ func CheckUser(ctx echo.Context) error {
 	result, err := services.CheckUser(uuid)
 	if err != nil {
 		log.Println(err)
-		return ctx.JSON(http.StatusInternalServerError, map[string]interface{}{
+		return ctx.JSON(http.StatusInternalServerError, echo.Map{
 			"status":  http.StatusInternalServerError,
 			"message": "予期しないエラーが発生しました",
 		})
 	}
 
 	// ユーザー情報を返す
-	return ctx.JSON(http.StatusOK, map[string]interface{}{
+	return ctx.JSON(http.StatusOK, echo.Map{
 		"status": http.StatusOK,
 		"data":   result,
 	})
@@ -42,7 +42,7 @@ func Signup(ctx echo.Context) error {
 	// リクエストを構造体にバインド
 	req := new(services.SignupRequest) // サービス層で定義した型を使う
 	if err := ctx.Bind(req); err != nil {
-		return ctx.JSON(http.StatusBadRequest, map[string]interface{}{
+		return ctx.JSON(http.StatusBadRequest, echo.Map{
 			"status":  http.StatusBadRequest,
 			"message": "リクエストの中身が不正です",
 		})
@@ -53,7 +53,7 @@ func Signup(ctx echo.Context) error {
 	if err != nil {
 		// カスタムエラーかチェック
 		if customErr, ok := err.(*custom_error.CustomError); ok {
-			return ctx.JSON(customErr.Code, map[string]interface{}{
+			return ctx.JSON(customErr.Code, echo.Map{
 				"status": customErr.Code,
 				"message":  customErr.Message,
 			})
@@ -61,14 +61,14 @@ func Signup(ctx echo.Context) error {
 
 		// 予期しないエラーの場合
 		fmt.Println(err)
-		return ctx.JSON(http.StatusInternalServerError, map[string]interface{}{
+		return ctx.JSON(http.StatusInternalServerError, echo.Map{
 			"status": http.StatusInternalServerError,
 			"message":  "予期しないエラーが発生しました",
 		})
 	}
 
 	// ユーザー情報を返す
-	return ctx.JSON(http.StatusCreated, map[string]interface{}{
+	return ctx.JSON(http.StatusCreated, echo.Map{
 		"status":  http.StatusCreated,
 		"message": "ユーザーを作成しました",
 		"user_id": result,
